16a50f1566bc2ca9838e162f0bea370cd10a73c9
comame <dev@comame.xyz>
2022-02-03 18:32:29 +0900

    ミウネルとリューゴンを追加

--- src/config/channels.ts
+++ src/config/channels.ts
@@ -2,4 +2,6 @@ export const channels = {
     '天野ピカミィ': 'UCajhBT4nMrg3DLS-bLL2RCg',
     '緋笠トモシカ': 'UC3vzVK_N_SUVKqbX69L_X4g',
     '磁富モノエ': 'UCaFhsCKSSS821N-EcWmPkUQ',
+    '羽渦ミウネル': 'UCE5VgVGRPfNCjXPeTe1QJHA',
+    '大門地リューゴン': 'UCivDgaCAh7WPBoKA24WNwJQ'
 }
